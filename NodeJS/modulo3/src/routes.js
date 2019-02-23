b8e996b6055a0f2124ac93403e717cd36c640d87
Vinícius Crepschi <vibracre@gmail.com>
2019-02-23 15:51:06 -0300

    Added module 3

---
+++ NodeJS/modulo3/src/routes.js
@@ -0,0 +1,21 @@
+/* eslint-disable new-cap */
+const express = require('express');
+const routes = express.Router();
+
+const controllers = require('./app/controllers');
+const authMiddleware = require('./app/middlewares/auth');
+
+routes.post('/users', controllers.UserController.store);
+routes.post('/sessions', controllers.SessionController.store);
+
+routes.use(authMiddleware);
+
+// Rotas de ads
+
+routes.get('/ads', controllers.AdController.index);
+routes.get('/ads/:id', controllers.AdController.show);
+routes.post('/ads', controllers.AdController.store);
+routes.put('/ads/:id', controllers.AdController.update);
+routes.delete('/ads/:id', controllers.AdController.destroy);
+
+module.exports = routes;
